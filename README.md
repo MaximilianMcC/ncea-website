3e38f656c0642d2a114d513381a401645a2c6cc4
Max <max@maximilian.co.nz>
2022-06-03 07:09:14 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+![Long bay college logo](https://www.longbaycollege.com/wp-content/uploads/2020/09/Long_Bay_College_Logo_Tag2-1024x141.png)
+
+# DTS NCEA Assessment - 91978
+I will be using this GitHub repo to keep track of the development of my website. My client is the Coatesville Collective and they need a website to display their music to people.
---
+++ script.js
@@ -0,0 +1,32 @@
+// Audio player
+const audioPlayer = document.querySelector(".preview-audio");
+const audioPlayPause = document.querySelector(".audio-play-pause");
+const audioPlayVolume = document.querySelector(".audio-volume");
+
+// Play or pause the audio preview
+//TODO: Add icons instead of text
+audioPlayPause.addEventListener("click", () => {
+
+    // Check for if the music is playing or paused
+    const status = audioPlayPause.getAttribute("currentState");
+
+    // Swap the positions and play/pause
+    if (status === "pause") {
+        audioPlayPause.setAttribute("currentState", "play");
+        audioPlayPause.innerHTML = "Pause";
+        audioPlayer.play();
+    }
+    else if (status === "play") {
+        audioPlayPause.setAttribute("currentState", "pause");
+        audioPlayPause.innerHTML = "Play";
+        audioPlayer.pause();
+    }
+
+});
+
+// Change the audio volume
+audioPlayVolume.addEventListener("input", (e) => {
+
+    audioPlayer.volume = e.target.value;
+
+});
