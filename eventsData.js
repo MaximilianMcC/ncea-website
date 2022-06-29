36915a6f934e38eb5294c98a4558415a31caaa14
Max <max@maximilian.co.nz>
2022-06-29 22:02:29 +1200

    Added event data JS

---
+++ eventsData.js
@@ -0,0 +1,21 @@
+//! This is not the best way to do this, however because I'm not using nodeJS I think that its ok. When using node JS I would do in JSON, MqSQL, MD, or something else
+
+// All of the events
+const eventData = [
+
+    {
+        // Event details
+        "title": "Example Title",
+        "information": "Example description filled with information",
+        "location": "Long Bay College",
+        "address": "30 Ashley Avenue, Long Bay, Auckland 0630"
+    },
+    {
+        // Event details
+        "title": "Example Title",
+        "information": "Example description filled with information",
+        "location": "Long Bay College",
+        "address": "30 Ashley Avenue, Long Bay, Auckland 0630"
+    }
+
+]
