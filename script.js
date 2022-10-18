9d1f6434f66f42d8d6bc03dd3a5d65c01e24828f
Max <max@maximilian.co.nz>
2022-10-18 22:15:12 +1300

    Made custom audio player

---
+++ script.js
@@ -0,0 +1,31 @@
+// Audio player
+const audioPlayerButton = document.querySelector(".audio-media-control");
+const audio = document.querySelector("audio");
+
+audioPlayerButton.addEventListener("click", () => {
+
+    // Get the current state of the player
+    const state = audioPlayerButton.getAttribute("state");
+    console.log(state);
+    
+    // Check for what the state is
+    if (state === "paused") {
+
+        // Change the state
+        audioPlayerButton.setAttribute("state", "playing");
+        audioPlayerButton.innerHTML = `<i class="fa-solid fa-pause"></i>`;
+
+        // Play the audio
+        audio.play();
+
+    } else if (state === "playing") {
+
+        // Change the state
+        audioPlayerButton.setAttribute("state", "paused");
+        audioPlayerButton.innerHTML = `<i class="fa-solid fa-play"></i>`;
+
+        // Pause the audio
+        audio.pause();
+    }
+
+});
